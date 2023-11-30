bd3f47173596fc35637b7555f0f14cb4ced1cc4f
Emil Karlström <emkl@WIN10-EMILK.localdomain>
2023-11-30 14:11:44 +0100

    Website can now be deployed in the kubernetes cluster

--- scripts/deployment.sh
+++ scripts/deployment.sh
@@ -11,24 +11,33 @@ NC='\033[0m'    # no color
 
 # Deletes all the K8 resources 
 deleteKubernetesResources() {
-    printf "${GREEN}Deleting all Kubernetes resouces${NC}\n"
-    objsToDrop=("services" "deployments" "configmaps")
+    read -p "This will attempt deleting ALL K8 resources [Y/N]: " mainmenuinput
 
-    for obj in ${objsToDrop[@]}
-    do
-        printf "${ORANGE}Deleting $obj ${NC}\n"
-        kubectl delete $obj --all 1> /dev/null
-    done
+    case $mainmenuinput in
+        "y" | "Y")
+            printf "${GREEN}Deleting all Kubernetes resouces${NC}\n"
+            objsToDrop=("services" "deployments" "configmaps")
 
-    printf "${CYAN}Deleting PresistentVolumeClaims...${NC}\n"
-    kubectl patch pvc postgres-pv-claim -p '{"metadata":{"finalizers":null}}' 1> /dev/null
-    kubectl delete pvc postgres-pv-claim --grace-period=0 --force 1> /dev/null
+            for obj in ${objsToDrop[@]}
+            do
+                printf "${ORANGE}Deleting $obj ${NC}\n"
+                kubectl delete $obj --all 1> /dev/null
+            done
 
-    printf "${CYAN}Deleting PresistentVolumes...${NC}\n"
-    kubectl patch pv postgres-pv-volume -p '{"metadata":{"finalizers":null}}' 1> /dev/null
-    kubectl delete pv postgres-pv-volume --grace-period=0 --force 1> /dev/null
+            printf "${CYAN}Deleting PresistentVolumeClaims...${NC}\n"
+            kubectl patch pvc postgres-pv-claim -p '{"metadata":{"finalizers":null}}' 1> /dev/null
+            kubectl delete pvc postgres-pv-claim --grace-period=0 --force 1> /dev/null
 
-    printf "${GREEN}Done${NC}\n"
+            printf "${CYAN}Deleting PresistentVolumes...${NC}\n"
+            kubectl patch pv postgres-pv-volume -p '{"metadata":{"finalizers":null}}' 1> /dev/null
+            kubectl delete pv postgres-pv-volume --grace-period=0 --force 1> /dev/null
+
+            printf "${GREEN}Done${NC}\n"
+            ;;
+        *)
+            printf "Exiting\n"
+            ;;
+    esac
 }
 
 # Deploys all the K8 resources
@@ -40,38 +49,62 @@ deployKubernetesResources()
     kubectl apply -f postgres-pvc-pv.yaml   
     kubectl apply -f postgres-service.yaml   
     kubectl apply -f postgres-deployment.yaml
+    
     # Read the database ip
     POSTGRES_IP=$(kubectl get service/postgres-service -o jsonpath='{.spec.clusterIP}')
-    printf "${BLUE}Postgres deployed. Binding postgres ip ($POSTGRES_IP) to configuration${NC}\n"
+    printf "${BLUE}postgres deployed. Binding postgres ip ($POSTGRES_IP) to movies-api ConfigMap${NC}\n"
 
     # Apply the database ip to the movie-service config
     # it's not great but it works for now :D 
     # Totally not stolen from the course so that's cool
     kubectl apply -f movies-config.yaml 2> /dev/null 
-    kubectl get configmap/movies-service-config -o yaml \
+    kubectl get configmap/movies-api-config -o yaml \
         | sed -r "s/NOTSET/$POSTGRES_IP/" | kubectl apply -f - 
 
-    # Deploy movies-service and load balancer
-    printf "${BLUE}Deploying movies-service...${NC}\n"
+    # Deploy movies-api and load balancer
+    printf "${BLUE}Deploying movies-api...${NC}\n"
     kubectl apply -f movies-deployment.yaml 
+
+    # Read the api ip
+    API_IP=$(kubectl get service/movies-api-balancer -o jsonpath='{.spec.clusterIP}')
+    printf "${BLUE}movies-api deployed. Binding API ip ($API_IP) to movies-frontend ConfigMap${NC}\n"
+    kubectl get configmap/movies-frontend-config -o yaml \
+        | sed -r "s/NOTSET/$API_IP/" | kubectl apply -f - 
+
+    printf "${BLUE}Deploying movies-frontend...${NC}\n"
+    kubectl apply -f frontend-deployment.yaml
 }
 
 # A short function that checks the second argument when invoked with "deploy"
 # to see if we want forward the services with minikube or kubectl, or not at all
 deployWith() 
 {
+    case $2 in
+        "backend" | "b")
+            service="movies-service-balancer"
+            ports="4000:4000"
+            ;;
+        *)
+            service="movies-frontend-balancer"
+            ports="8000:8000"
+            ;;
+            
+    esac
+
     case $1 in
         "minikube" | "mk")
             if command -v minikube &> /dev/null
             then 
-                printf "${BLUE}Found minikube installation, creating minikube url connection${NC}\n"
-                minikube service movies-service-balancer --url
+                printf "${BLUE}Found minikube installation, forwarding ${service}  with minikube url connection${NC}\n"
+                minikube service ${service} --url
             else
                 printf "${RED}Could not find command \"minikube\"${NC}\n"
             fi
             ;;
         "kubectl" | "k")
-            kubectl port-forward service/movies-service-balancer 4000:4000
+            printf "${CYAN}Forwarding ${service} with kubectl...\n${ORANGE}"
+            kubectl port-forward service/$service $ports
+            printf "${NC}"
             ;;
     esac
 }
@@ -101,5 +134,5 @@ case $1 in
         deployKubernetesResources
         ;;
     "forward")
-        deployWith $2
+        deployWith $2 $3
 esac
